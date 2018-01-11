a4fce46de61061ead24ebe6c2310578785ae23b3
ahmadrockets <ahmadrockets@gmail.com>
2018-01-11 23:53:43 +0700

    inserting data

--- insert_data.py
+++ insert_data.py
@@ -1,13 +1,20 @@
 import MySQLdb as mdb
 import sys
+import datetime
 
 db  = mdb.connect('localhost', 'root', '', 'python')
 cur = db.cursor()
 
 try:
-    sql = "insert"
+    date = datetime.datetime(1995, 10, 10)
+    sql = "INSERT INTO orang (nama, alamat, tgl_lahir) VALUES ('%s', '%s', '%s')" % ('Prasetyo Nugroho', 'Jl Ahmad Yani', date)
+    cur.execute(sql)
+    db.commit()
+    print("Berhasil input data")
+    print (cur._last_executed)
 except :
+    db.rollback()
+    print(cur._last_executed)
     print("Gagal insert data")
-
 if db:
     db.close()
